05a3ccb458a6b1f90341a8cfc502946afc33ce46
DanyPlaya <danila.lusuy@gmail.com>
2023-11-02 20:29:45 +0300

    fix: remade headerItemPopover for shandcn

--- src/widgets/header/ui/headerItemPopover.tsx
+++ src/widgets/header/ui/headerItemPopover.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Separator } from "@/shared/ui/separator";
 import { HeaderItemTypes } from "../types/headerItem";
 
 type HeaderItemPopoverType = {
@@ -8,19 +9,17 @@ type HeaderItemPopoverType = {
 export const HeaderItemPopover = (props: HeaderItemPopoverType) => {
   const { items } = props;
   return (
-    <div className="mr-2 mt-2">
-      <div className=" rounded-xl relative border-2 border-solid border-red-200 bg-white p-2 sm:grid-cols-2 w-60">
-        {items.map((item) => (
-          <Link
-            className="flex p-3 lg:hover:bg-orange-300 rounded-xl"
-            href={item.href}
-            key={item.href}
-          >
-            <Image className="mr-2" src={item.icon} alt={item.text} />
-            <p>{item.text}</p>
-          </Link>
-        ))}
-      </div>
-    </div>
+    <>
+      {items.map((item) => (
+        <Link
+          className="flex p-3 lg:hover:bg-orange-300 rounded-xl"
+          href={item.href}
+          key={item.href}
+        >
+          <Image className="mr-2" src={item.icon} alt={item.text} />
+          <p>{item.text}</p>
+        </Link>
+      ))}
+    </>
   );
 };
