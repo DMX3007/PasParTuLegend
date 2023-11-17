8ef78f2c539051a773359b7f22503ce8bebaea2d
DanyPlaya <danila.lusuy@gmail.com>
2023-11-17 15:10:36 +0300

    feat: wrapped the entire application in layout

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,16 @@
+import { useRouter } from "next/router";
 import { AppProps } from "next/app";
+import { Header } from "@/widgets";
+import { Layout } from "@/shared/ui/layouts";
 
 export function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  return (
+    <>
+      <Layout header={<Header />} >
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
