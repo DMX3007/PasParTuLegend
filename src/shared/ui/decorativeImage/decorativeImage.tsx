38967adc105e176c4646378d211fe008028933ef
earielle <demix3991@gmail.com>
2024-01-26 14:42:22 +0700

    fix: cards height

--- src/shared/ui/decorativeImage/decorativeImage.tsx
+++ src/shared/ui/decorativeImage/decorativeImage.tsx
@@ -15,13 +15,15 @@ export const DecorativeImage = ({ className, src, alt, text, back }: DecorativeI
   return (
     <>
       {!back ?
-        <div className={twMerge("relative flex flex-col items-center justify-center rounded-lg max-w-[75px] max-h-[75px] sm:max-w-[100px] sm:max-h-[100px] transition-all duration-500 bg-white shadow-sm ", className)} >
-          <Image
-            className={"mt-[5px] pb-[5px] rounded-[10px]"}
-            src={src!}
-            alt={alt}
-          ></Image>
-          <p className={twMerge(comfortaa.className, "font-bold text-sm uppercase")}>{text}</p>
+        <div className={twMerge("flex flex-col items-center justify-center rounded-lg max-w-[75px] max-h-[75px] sm:max-w-[75px] sm:max-h-[75px] min-w-[75px] min-h-[85px] sm:min-w-[75px] sm:min-h-[85px]        transition-all duration-500 bg-white shadow-sm ", className)} >
+          <div className="max-w-[55px] max-h-[85px] text-center">
+            <Image
+              className={"mt-[5px] pb-[5px] rounded-[10px]"}
+              src={src!}
+              alt={alt}
+            ></Image>
+            <p className={twMerge(comfortaa.className, "font-bold text-sm uppercase")}>{text}</p>
+          </div>
         </div> :
         <div className={twMerge("flex flex-col items-center justify-center w-24 h-24 min-w-15 min-h-15 sm:w-16 sm:h-16 gap-1 bg-gray-100 shadow-sm", className)}>
           {back}
